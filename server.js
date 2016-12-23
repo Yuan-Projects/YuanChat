f9f3631cf4ba0aef74c7e18f72b0ef0aa5e6464a
rainyjune <rainyjune@live.cn>
2016-12-23 16:30:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const WebSocketServer = require('ws').Server,
       wss = new WebSocketServer({port: 8181});
       
+var defaultMessages = ['Hello, world!', 'Test 1', 'Test 2'];
+      
 wss.on('connection', (ws)=> {
   console.log('Client connected');
+  
   ws.on('message', (message) => {
-    console.log(message);
+    var request = JSON.parse(message);
+    switch(request.type) {
+      case 'fetchDefault':
+        ws.send(JSON.stringify(defaultMessages));
+        break;
+      case 'createMessage':
+        wss.clients.forEach(function each(client) {
+          var data = [request.data];
+          client.send(JSON.stringify(data));
+        });
+        break;
+    }
   });
+  
 });
